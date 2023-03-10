2c3fd2e7701073b10cf7403c7b40b2608fdfeb63
Osman <114867365+osmansam@users.noreply.github.com>
2023-03-09 21:43:35 -0500

    form row and neccessary components created

---
+++ src/pages/Register.js
@@ -0,0 +1,126 @@
+import { useState, useEffect } from "react";
+import FormRow from "../components/FormRow";
+import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import { registerUser, loginUser } from "../features/user/userSlice";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
+
+const initialState = {
+  name: "",
+  lastName: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
+function Register() {
+  const [values, setValues] = useState(initialState);
+  const { user, isLoading } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({ ...values, [name]: value });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, password, isMember, lastName } = values;
+    if (!email || !password || (!isMember && (!name || !lastName))) {
+      toast.error("Please fill out all fields");
+      return;
+    }
+    if (isMember) {
+      dispatch(loginUser({ email: email, password: password }));
+      return;
+    }
+    dispatch(registerUser({ name, email, password, lastName }));
+    setValues({ ...values, isMember: true });
+  };
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        history.push("/");
+      });
+    }
+  }, [user]);
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {/* name field */}
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+        {/* last name field */}
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="lastName"
+            value={values.lastName}
+            handleChange={handleChange}
+          />
+        )}
+        {/* email field */}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password field */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type="submit" className="btn btn-block" disabled={isLoading}>
+          {isLoading ? "loading..." : "submit"}
+        </button>
+
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
+}
+const Wrapper = styled.section`
+  display: grid;
+  align-items: center;
+  .logo {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+  }
+  .form {
+    max-width: 400px;
+    border-top: 5px solid var(--primary-500);
+    margin: 0 auto;
+  }
+
+  h3 {
+    text-align: center;
+  }
+  p {
+    margin: 0;
+    margin-top: 1rem;
+    text-align: center;
+  }
+`;
+export default Register;
